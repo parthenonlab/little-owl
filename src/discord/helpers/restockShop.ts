9ede5d86e6a367ff6438bc04cb6da0e6c99822fb
Athena <athena@parthenon.app>
2026-03-31 10:10:03 -0700

    [OWL-00029] Updated helpers with missing jsdocs

--- src/discord/helpers/restockShop.ts
+++ src/discord/helpers/restockShop.ts
@@ -1,5 +1,10 @@
 import { BotState } from '@/interfaces/bot';
 
+/**
+ * Restocks pokeballs, greatballs, and ultraballs to their daily limits.
+ *
+ * @param state - The current bot state.
+ */
 export const restockShopDaily = (state: BotState) => {
   state.shop.pokeball = 250;
   state.shop.greatball = 100;
@@ -7,6 +12,11 @@ export const restockShopDaily = (state: BotState) => {
   state.shop.lastDailyRestock = new Date();
 };
 
+/**
+ * Restocks masterballs to their weekly limit.
+ *
+ * @param state - The current bot state.
+ */
 export const restockShopWeekly = (state: BotState) => {
   state.shop.masterball = 1;
   state.shop.lastWeeklyRestock = new Date();
